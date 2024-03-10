fee4e71e39c87c868f7a3951529e3521230563ee
Jonas <jona813k@hotmail.com>
2024-03-10 16:42:10 +0100

    Start

---
+++ Login/Login/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Login
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
